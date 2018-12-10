c174b53d5ce956af05dd95ed000aa8e89052aeae
ak <ak@proekspert.ee>
2018-12-10 17:15:11 +0200

    New version

--- Formula/kogu.rb
+++ Formula/kogu.rb
@@ -1,8 +1,8 @@
 class Kogu < Formula
   desc "Kogu toolset for tracking data science projects"
   homepage "https://kogu.io"
-  url "https://github.com/kogu-io/kogu/releases/download/v0.3.4.99/kogu_0.3.4.99_macos_amd64.zip"
-  sha256 "d222937dabf18fa0b5efc7fbcda3507cc7a6640498186dfcc9e5676142d591e6"
+  url "https://github.com/kogu-io/kogu/releases/download/v0.3.4.102/kogu_0.3.4.102_macos_amd64.zip"
+  sha256 "b60311845d395b2ef951d48219656b43d18ca70170d54e6c0840092b34382019"
 
   def install
     bin.install "kogu-server"
